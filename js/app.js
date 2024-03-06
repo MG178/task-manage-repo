a5708e67a6853c8cff575d33cca9eb016f0fa0c8
Gurdip Bath <bathgurdip1@gmail.com>
2024-03-06 19:30:58 +0000

    added ReactDOM.render into my code

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+const navbar =(
+    <nav>
+    <><h1>High Converting Sites</h1><ul>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Pricing</li>
+    </ul></>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
