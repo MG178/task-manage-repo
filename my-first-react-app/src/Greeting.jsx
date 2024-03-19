57a074a4a1fd5a4517110ddd95b9bdce0ec92f01
Gurdip Bath <bathgurdip1@gmail.com>
2024-03-19 18:42:18 +0000

    add react img

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -1,5 +1,23 @@
-function Greeting() {
-    return <h1>this is my react component</h1>;
-}
+import React from 'react';
+
+const Greeting = () => {
+  return (
+    <div>
+      <img src="./react img.jpg" width="200px" alt ="react logo"/>
+      <h1>Test title</h1>
+      <ol className="test-list">
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+      </ol>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
 
 export default Greeting;
