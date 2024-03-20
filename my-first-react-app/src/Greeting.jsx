1844327a381c8f5b80971db3eb79671f5704bf49
Gurdip Bath <bathgurdip1@gmail.com>
2024-03-20 19:45:50 +0000

    add footer to Greeting.jsx file

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -6,7 +6,6 @@ const Greeting = () => {
       <header>
         <nav>
           <img src="./react img.jpg" width="200px" alt ="react logo"/>
-
         </nav>
       </header>
       <h1>A list created using react</h1>
@@ -15,12 +14,16 @@ const Greeting = () => {
         <li>List item 2</li>
         <li>List item 3</li>
       </ol>
+      <footer>
+      </footer>
       <svg>
         <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
       </svg>
       <form>
         <input type="text" />
       </form>
+      <small>2024 GSB development. All rights reserved.</small>
+
     </div>
   );
 };
